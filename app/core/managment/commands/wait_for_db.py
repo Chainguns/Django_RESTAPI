bc4c29ec3724689f4437092fef917679b87403cc
Daniel <danielhirshson@gmail.com>
2021-11-17 15:42:17 +0200

    Fix

--- app/core/managment/commands/wait_for_db.py
+++ app/core/managment/commands/wait_for_db.py
@@ -1,18 +1,22 @@
 import time
-from django.db import connections
 
+from psycopg2 import OperationalError as Psycopg2OpError
+
+from django.db import connections
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
-    """ DJangop command to pause execution until db is available"""
+    """Django command to pause execution until database is available"""
+
     def handle(self, *args, **options):
         self.stdout.write('Waiting for database...')
         db_conn = None
         while not db_conn:
             try:
                 db_conn = connections['default']
-            except OperationalError:
+            except (OperationalError, Psycopg2OpError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
 
