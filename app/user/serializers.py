a2b27096210fdaf2e7a1c7ec19db92364149bbb0
Daniel <danielhirshson@gmail.com>
2021-11-20 14:40:23 +0200

    Added manage user endpoint (edit user personal settings)

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -14,6 +14,16 @@ class UserSerialzer(serializers.ModelSerializer):
         ''' Create a new user with encrypted password and return it '''
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """Update a user, and return it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 class AuthTokenSerializer(serializers.Serializer):
     """ Serializer for the user authentication object """
     email = serializers.CharField()
@@ -23,7 +33,7 @@ class AuthTokenSerializer(serializers.Serializer):
     )
 
     def validate(self, attrs):
-        """ Validate and authinticate the user """
+        """ Validate and authenticate the user """
         email = attrs.get('email')
         password = attrs.get('password')
 
--- app/user/test/test_user_api.py
+++ app/user/test/test_user_api.py
@@ -8,12 +8,13 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
+ME_URL = reverse('user:me')
 
 def create_user(**params):
     return get_user_model().objects.create_user(**params)
 
 class PublicUserApiTests(TestCase):
-    """ Tests the users API (public = no authntication needed) """
+    """ Tests the users API (public = no authentication needed) """
 
     def setUp(self):
         self.client = APIClient()
@@ -102,3 +103,49 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(TOKEN_URL, {'email': 'one', "password": ''})
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+    def test_retrieve_user_unauthorized(self):
+        """ Test that authentication is required for users"""
+        res = self.client.get(ME_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+class PrivateUserApiTests(TestCase):
+    """ Test API requests that require authentication"""
+
+    def setUp(self):
+        self.user = create_user(
+        email='test@django.com',
+        password='testpass',
+        name='name'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+
+    def test_retrive_profile_success(self):
+        """ Tests that profile of logged in user was retrieved"""
+        res = self.client.get(ME_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+        'email': self.user.email ,
+        'name': self.user.name
+        })
+
+    def test_post_me_not_allowed(self):
+        """test that post is not allowed on the me url"""
+        res = self.client.post(ME_URL, {})
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_user_profile_update(self):
+        """test that user profile was updated"""
+
+        payload = {
+        'name' : 'new name',
+        'password': 'newpassword123'
+        }
+        res = self.client.patch(ME_URL, payload)
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password, payload['password'])
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
